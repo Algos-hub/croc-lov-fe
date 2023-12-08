a02e460aea2a5801b4d3b1d69f82ed878cb97d4f
Algos-hub <72273184+Algos-hub@users.noreply.github.com>
2023-12-08 12:34:12 +0100

    Account page skeleton finished

--- screens/AccountScreen.jsx
+++ screens/AccountScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, StatusBar, Appearance, Image, StyleSheet } from "react-native";
-import { Text, IconButton } from "react-native-paper";
+import { Text, Button } from "react-native-paper";
 
 // Import custom theme
 import lightTheme from "../theme/lightTheme";
@@ -21,7 +21,39 @@ export default function AccountScreen() {
       <StatusBar
         barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
       />
-      <Text>AccoutScreen</Text>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={require("../assets/background.jpg")}
+            resizeMode="cover"
+            style={{
+              height: 150,
+              width: 150,
+              borderRadius: 100,
+              marginBottom: 20,
+            }}
+          />
+          <Text variant="headlineMedium">ACCOUNT_NAME</Text>
+        </View>
+        <View
+          style={{
+            alignItems: "center",
+          }}
+        >
+          <Button mode="contained-tonal" style={{ marginBottom: 40 }}>
+            Delete Account
+          </Button>
+          <Button mode="contained" theme={{ colors: colorTheme }}>
+            Log Out
+          </Button>
+        </View>
+      </View>
     </View>
   );
 }
@@ -30,5 +62,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    justifyContent: "center",
   },
 });

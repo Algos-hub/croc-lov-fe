e53dc0668607408eb216a5b364556b378c463879
Algos-hub <72273184+Algos-hub@users.noreply.github.com>
2023-12-14 11:11:56 +0100

    EAS settings

--- app.config.js
+++ app.config.js
@@ -1,40 +1,50 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
-  "expo": {
-    "name": "croc-lov",
-    "slug": "croc-lov",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "automatic",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  expo: {
+    owner: "pedrofmf",
+    name: "croc-lov",
+    slug: "croc-lov",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    "assetBundlePatterns": ["**/*"],
-    "ios": {
-      "supportsTablet": true
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
     },
-    "android": {
-      "package": "com.project.croc_lov",
-      "versionCode": 1,
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
-      }
+    android: {
+      package: "com.project.croc_lov",
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
+    web: {
+      favicon: "./assets/favicon.png",
     },
-    "extra": {
+    extra: {
       firebaseApiKey: process.env.FIREBASE_API_KEY,
       firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
       firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
       firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID
-    }
-  }
-}
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      eas: {
+        projectId: "e336689d-0471-4174-96a9-a7383619fbba",
+      },
+    },
+    updates: {
+      url: "https://u.expo.dev/e336689d-0471-4174-96a9-a7383619fbba",
+    },
+    runtimeVersion: {
+      policy: "appVersion",
+    },
+  },
+};
